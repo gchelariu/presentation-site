bf233279b2d13ef956249d0f9fcc3821884106fa
Gabriel <gabrielcchelariu@gmail.com>
2020-09-25 15:17:23 +0300

    Nav version 2.0

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// select element function
+const selectElement = function (element) {
+    return document.querySelector(element);
+};
+
+const menuToggler = selectElement('.menu-toogle');
+const body = selectElement('body');
+
+menuToggler.addEventListener('click', function(){
+    body.classList.toggle('open');
+});
